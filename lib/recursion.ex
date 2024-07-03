b2f6b181452f82ed988dc39051acdba9b7e39701
nigelyegon <nigelkimutai@gmail.com>
2024-07-03 08:13:34 +0300

    recursion

---
+++ lib/recursion.ex
@@ -0,0 +1,26 @@
+defmodule Recursion do
+  @moduledoc """
+  Documentation for `Recursion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recursion.hello()
+      :world
+
+  """
+ @doc """
+
+ """
+  def elixsir_list() do
+    ["phone", "laptop","sketchpad","markers","other stuff"]
+  end
+
+  def  do
+
+  end
+
+end
